2f6534e019e56692e5f73663a5afdd8d2858f2b0
mattlowese <141253110+mattlowese@users.noreply.github.com>
2024-01-09 23:24:55 -0500

    SHIP-5 Fix autoload error, put back removed code in Router

--- app/Router.php
+++ app/Router.php
@@ -1,21 +1,17 @@
-<?php 
+<?php
 
-//Strict type checking
 declare(strict_types=1);
 
 //Defines the namespace for the class
 namespace App;
 
-//Import Exceptions class (currently not made)
 use App\Exceptions\RouterNotFoundException;
 
-class Router {
-
-    //Private property - array - store information about routes
-    private array $routes;
+class Router 
+{
+    private array $routes = [];
 
     /**
-     * Register a new route with its associated action.
      *
      * @param string $requestMethod The HTTP request method ex: GET/POST
      * @param string $route         The URI route to be registered.
@@ -23,14 +19,74 @@ class Router {
      *
      * @return $this
      */
-    
-    public function register(string $requestMethod, string $route, callable|array $action) 
+    public function register(string $requestMethod, string $route, callable|array $action): self
     {
         // Store the provided action for the given route and request method.
         $this->routes[$requestMethod][$route] = $action;
 
-        // Allow method chaining by returning the current instance of the class.
         return $this;
     }
 
+    /**
+     *
+     * @param string $route         The URI route to be registered.
+     * @param callable|array $action The action associated with the route (callback function or controller/method array).
+     *
+     * @return $this
+     */
+    public function get(string $route, callable|array $action): self 
+    {
+        // Calls register method, using 'get' as the request method.
+        return $this->register('get', $route, $action);
+    }
+
+    /**
+     * Register a new route with its associated action for the HTTP POST method.
+     *
+     * @param string $route         The URI route to be registered.
+     * @param callable|array $action The action associated with the route (callback function or controller/method array).
+     *
+     * @return $this
+     */
+    public function post(string $route, callable|array $action): self 
+    {
+        // Calls register method, using 'post' as the request method.
+        return $this->register('post', $route, $action);
+    }
+
+    public function routes(): array 
+    {
+        return $this->routes;
+    }
+    
+    public function resolve(string $requestUri, string $requestMethod) 
+    {
+        $route = explode('?', $requestUri)[0];
+        $action = $this->routes[$requestMethod][$route] ?? null;
+    
+        if (! $action) 
+        {
+            throw new RouterNotFoundException("Route not found for {$requestMethod} {$route}");
+        }
+    
+        if (is_callable($action))
+        {
+            return call_user_func($action);
+        }
+    
+        if (is_array($action))
+        {
+            [$class, $method] = $action;
+    
+            if (class_exists($class))
+            {
+                $class = new $class();
+    
+                if (method_exists($class, $method))
+                {
+                    return call_user_func_array([$class, $method], []);
+                }
+            }
+        }
+    }
 }
